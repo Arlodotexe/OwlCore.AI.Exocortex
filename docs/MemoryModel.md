7a3a508c3642b4b09c863e3782957f3aa5f96d11
Arlo Godfrey <arlo.godfrey@outlook.com>
2023-08-24 21:20:49 -0500

    Improved docs

---
+++ docs/MemoryModel.md
@@ -0,0 +1,39 @@
+# Exocortex Memory Model
+
+The curves for short-term and long-term memories are exponential and logarithmic respectively, normalizing and adapting to both the oldest long-term memory and the short-term memory duration specified by the user.
+
+## Decay Rate and Threshold functions 
+
+**Set by the end user**:
+- Short-term memory duration 
+- Long-term decay threshold
+- The rest is calculated based on these two values
+
+**Short-term Memory Decay**:
+- **Function**: Exponential decay
+- **Equation**: \( short_term_decay(t)=exp(−short_term_decay_rate×t) \)
+- **Rationale**: Captures the rapid fading of recent memories, consistent with how short-term human memories fade quickly.
+   
+**Long-term Memory Decay**:
+- **Function**: Reversed logarithmic decay
+- **Rationale**: Mimics human memory patterns where memories fade slowly over time but never completely disappear. The initial rapid decay slows down, representing the long-lasting nature of certain memories.
+   
+**Short-term Decay Threshold**:
+
+- **Value**: Computed exponentially using the long term duration.
+- **Function**: Exponential
+- **Role**: Represents the point at which short-term memories start transitioning to long-term memories. As more long-term memories accumulate, the threshold decreases, leading short-term memories to decay faster.
+- **Challenge & Solution**: 
+    - It was observed after computing this based on long-term duration that the max possible threshold (1) was being approached exponentially with age, leading to rapid memory loss around age 45.
+    - The issue stems from how we dynamically adjust the short-term decay threshold based on the long-term duration. As the long-term duration increases (i.e., as someone gets older), the short-term decay threshold increases, which leads to a noticeable decay of short-term memory.
+    - The exponential function is chosen because it is the inverse of the logarithmic function used to calculate the long-term memory decay curve. This means that the difference between the short-term decay threshold and the long-term decay threshold is approached logistically, which results in a flattening and decline in long-term memory strength.
+
+ **Long-term Decay Threshold**:
+- **Value**: Constant at 0.01, can be changed by user.
+- **Function**: Exponential
+- **Role**: Represents the minimum strength a long-term memory can have. It's the point below which a memory is considered lost or inaccessible.
+- **Rationale**: Mimics the idea that some human memories, no matter how old, never completely vanish but can become very hard to access.
+- **Challenge & Solution**:
+  - By making the short-term decay threshold climb exponentially with age (countering the age 45 memory loss), we noted the functional inverse was happening - the difference between the short term decay threshold and the long-term decay threshold was being approached logistically.
+  - This resulted in a flattening and decline in long-term memory strength. This was rectified by applying an exponential function to counter it.
+   
---
+++ docs/main.py
@@ -0,0 +1,193 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Constants
+LONG_TERM_DECAY_THRESHOLD = 0.01
+
+# Decay functions
+# This function models the short-term memory decay using an exponential function. 
+# The decay rate is dynamically adjusted based on the short-term duration and the decay threshold.
+def short_term_decay(t, short_term_decay_rate):
+    """Compute the short-term decay."""
+    return np.exp(-short_term_decay_rate * t)
+
+# This function models the long-term memory decay using a reversed logarithmic function.
+# The decay is influenced by three parameters: a, b, and c.
+# 'a' is the initial memory strength, 'b' determines the rate of decay, 
+# and 'c' scales the time variable to adjust how quickly the function approaches the decay threshold.
+def long_term_decay(t, a, b, c):
+    """Compute the adjusted long-term decay using a reversed logarithmic function."""
+    return a - b * np.log(c * t + 1)
+
+# This function computes the short-term decay threshold using a logarithmic function.
+# The threshold represents the point at which short-term memory starts to transition to long-term memory.
+# The function ensures that as more long-term memories accumulate, 
+# the threshold decreases, meaning short-term memories decay faster.
+def compute_short_term_decay_threshold(long_term_duration):
+    """Compute the short-term decay threshold based on a logarithmic function."""
+    return 1 / (0.00005 * long_term_duration + 1)
+
+def compute_short_term_decay_threshold(long_term_duration):
+    """Compute the short-term decay threshold based on an exponential function."""
+    k = 0.00001
+    return 0.2 + 0.8 * np.exp(-k * long_term_duration)
+
+# Parameters for the plots
+short_term_duration = 5  # in minutes
+short_term_duration_hours = short_term_duration / 60  # Convert to hours
+
+# Given long-term durations in hours for different life stages
+long_term_durations_in_hours = {
+    "Infancy to Early Childhood": 5 * 365 * 24,
+    "Childhood to Adolescence": (18 - 5) * 365 * 24,
+    "Young Adulthood": (30 - 18) * 365 * 24,
+    "Middle Age": 40 * 365 * 24,
+    "Elderly": 70 * 365 * 24
+}
+
+# List to store file paths for generated plots
+adjusted_plot_paths = []
+
+# Create plots for each long-term duration
+for title, long_term_duration in long_term_durations_in_hours.items():
+    
+    # Create figure with 2x3 layout
+    fig, axes = plt.subplots(3, 2, figsize=(21, 12))
+    fig.suptitle(f"Memory Decay Over Time - {title}", fontsize=16)
+
+    # Calculate short-term decay threshold
+    short_term_decay_threshold = compute_short_term_decay_threshold(long_term_duration)
+    short_term_decay_rate = -np.log(short_term_decay_threshold) / short_term_duration_hours
+
+    # Parameters for the reversed logarithmic long-term decay
+    a = short_term_decay_threshold
+    b = 1
+    c = 1 / (long_term_duration / np.log(1 / LONG_TERM_DECAY_THRESHOLD - 1))
+    
+    # Parameters for the reversed logarithmic long-term decay
+    a = short_term_decay_threshold  # Start from where short-term decay ends
+    b = (a - LONG_TERM_DECAY_THRESHOLD) / np.log(long_term_duration)  # Adjust to approach the threshold at max duration
+    c = 1  # A default value, can be adjusted if needed
+    
+    # Get the value at the end of the short term duration to use as a factor for long term decay.
+    transition_strength = short_term_decay(short_term_duration_hours, short_term_decay_rate)
+    
+    # Time arrays for plots
+    t_short_1_5x = np.linspace(0, 1.5 * short_term_duration_hours, 1000)
+    t_short_5x = np.linspace(0, 5 * short_term_duration_hours, 1000)
+    t_long_0_5x = np.linspace(long_term_duration - 0.5 * long_term_duration, long_term_duration, 1000)  # Last 0.5x of long-term duration
+    t_long_10th_year = np.linspace(10 * 365 * 24 - 365 * 24, 10 * 365 * 24, 1000)  # 10th year
+    t_full_span = np.linspace(0, long_term_duration, 1000)
+    
+    # Modify the decay functions to handle the transition between short-term and reversed-log long-term decay
+    def memory_strength_over_time(t, short_term_decay_rate, a, b, c, short_term_duration_hours, transition_strength):
+        """Compute the memory strength over time, transitioning from short-term to long-term decay."""
+        # Short-term decay for times <= short-term duration
+        short_term_strength = short_term_decay(t, short_term_decay_rate)
+        
+        # Long-term decay for times > short-term duration
+        adjusted_t = t - short_term_duration_hours
+        offset = transition_strength - long_term_decay(0, a, b, c)
+        long_term_strength = long_term_decay(adjusted_t, a, b, c) + offset
+        
+        # Combine the two using a piecewise function
+        combined_strength = np.where(t <= short_term_duration_hours, short_term_strength, long_term_strength)
+        
+        return np.maximum(LONG_TERM_DECAY_THRESHOLD, combined_strength)
+
+    # Modify the memory strength arrays computation
+    memory_strength_short_1_5x = memory_strength_over_time(t_short_1_5x, short_term_decay_rate, a, b, c, short_term_duration_hours, transition_strength)
+    memory_strength_short_5x = memory_strength_over_time(t_short_5x, short_term_decay_rate, a, b, c, short_term_duration_hours, transition_strength)
+    memory_strength_long_0_5x = memory_strength_over_time(t_long_0_5x, short_term_decay_rate, a, b, c, short_term_duration_hours, transition_strength)
+    memory_strength_long_10th_year = memory_strength_over_time(t_long_10th_year, short_term_decay_rate, a, b, c, short_term_duration_hours, transition_strength)
+    memory_strength_full_span = memory_strength_over_time(t_full_span, short_term_decay_rate, a, b, c, short_term_duration_hours, transition_strength)
+
+    # Plotting
+    # 1.5x short term duration
+    axes[0][0].plot(t_short_1_5x, memory_strength_short_1_5x, label="Memory Strength", color="blue")
+    axes[0][0].axvline(x=short_term_duration_hours, color="red", linestyle="--", label="End of Short-Term Duration")
+    axes[0][0].axhline(y=short_term_decay_threshold, color="green", linestyle="--", label="Short-Term Decay Threshold")
+    axes[0][0].axhline(y=LONG_TERM_DECAY_THRESHOLD, color="purple", linestyle="--", label="Long-Term Decay Threshold")
+    axes[0][0].set_ylim([0, 1])
+    axes[0][0].set_xlim([0, 1.5 * short_term_duration_hours])
+    axes[0][0].set_title(f"Memory Decay over 1.5x Short-Term Duration")
+    axes[0][0].set_xlabel("Time (hours)")
+    axes[0][0].set_ylabel("Memory Strength")
+    axes[0][0].legend()
+
+    # 5x short term duration
+    axes[0][1].plot(t_short_5x, memory_strength_short_5x, label="Memory Strength", color="blue")
+    axes[0][1].axvline(x=short_term_duration_hours, color="red", linestyle="--", label="End of Short-Term Duration")
+    axes[0][1].axhline(y=short_term_decay_threshold, color="green", linestyle="--", label="Short-Term Decay Threshold")
+    axes[0][1].axhline(y=LONG_TERM_DECAY_THRESHOLD, color="purple", linestyle="--", label="Long-Term Decay Threshold")
+    axes[0][1].set_ylim([0, 1])
+    axes[0][1].set_xlim([0, 5 * short_term_duration_hours])
+    axes[0][1].set_title(f"Memory Decay over 5x Short-Term Duration")
+    axes[0][1].set_xlabel("Time (hours)")
+    axes[0][1].set_ylabel("Memory Strength")
+    axes[0][1].legend()
+
+
+    # 0.5x long term duration
+    axes[1][0].plot(t_long_0_5x, memory_strength_long_0_5x, label="Memory Strength", color="blue")
+    axes[1][0].axhline(y=LONG_TERM_DECAY_THRESHOLD, color="purple", linestyle="--", label="Long-Term Decay Threshold")
+    axes[1][0].axvline(x=long_term_duration, color="red", linestyle="--", label="End of Long-Term Duration")
+    axes[1][0].set_ylim([0, 1])
+    axes[1][0].set_xlim([long_term_duration - 0.5 * long_term_duration, long_term_duration])
+    axes[1][0].set_xticks(np.linspace(long_term_duration - 0.5 * long_term_duration, long_term_duration, 6))
+    axes[1][0].set_title("Memory Decay over Last 0.5x Long-Term Duration")
+    axes[1][0].set_xlabel("Time (years)")
+    axes[1][0].set_ylabel("Memory Strength")
+    axes[1][0].legend()
+
+    # Convert x-ticks from hours to years for better readability
+    x_ticks_hours = axes[1][0].get_xticks()
+    x_ticks_years = x_ticks_hours / (365 * 24)
+    axes[1][0].set_xticks(x_ticks_hours)
+    axes[1][0].set_xticklabels(np.round(x_ticks_years, 2))
+
+    # 10th year
+    axes[1][1].plot(t_long_10th_year, memory_strength_long_10th_year, label="Memory Strength", color="blue")
+    axes[1][1].axhline(y=short_term_decay_threshold, color="green", linestyle="--", label="Short-Term Decay Threshold")
+    axes[1][1].axhline(y=LONG_TERM_DECAY_THRESHOLD, color="purple", linestyle="--", label="Long-Term Decay Threshold")
+    axes[1][1].axvline(x=long_term_duration, color="red", linestyle="--", label="End of Long-Term Duration")
+    axes[1][1].set_ylim([0, 1])
+    axes[1][1].set_xlim([10 * 365 * 24 - 365 * 24, 10 * 365 * 24])
+    axes[1][1].set_xticks(np.linspace(10 * 365 * 24 - 365 * 24, 10 * 365 * 24, 13))
+    axes[1][1].set_xticklabels([f"{(i * 2) - i}" for i in range(0, 13)])
+    axes[1][1].set_title("Memory Decay over Last Year")
+    axes[1][1].set_xlabel("Time (months)")
+    axes[1][1].set_ylabel("Memory Strength")
+    axes[1][1].legend()
+
+    # Zoomed-out plot covering the full span of the long-term duration
+    axes[2][0].plot(t_full_span, memory_strength_full_span, label="Memory Strength", color="blue")
+    axes[2][0].axhline(y=short_term_decay_threshold, color="green", linestyle="--", label="Short-Term Decay Threshold")
+    axes[2][0].axhline(y=LONG_TERM_DECAY_THRESHOLD, color="purple", linestyle="--", label="Long-Term Decay Threshold")
+    axes[2][0].axvline(x=short_term_duration_hours, color="red", linestyle="--", label="End of Short-Term Duration")
+    axes[2][0].axvline(x=long_term_duration, color="red", linestyle="--", label="End of Long-Term Duration")
+    axes[2][0].set_ylim([0, 1])
+    axes[2][0].set_xlim([0, long_term_duration])
+    axes[2][0].set_title("Memory Decay over Full Span")
+    axes[2][0].set_xlabel("Time (years)")
+    axes[2][0].set_ylabel("Memory Strength")
+    axes[2][0].legend()
+
+    # Convert x-ticks from hours to years for better readability
+    x_ticks_hours_full = np.linspace(0, long_term_duration, 11)
+    x_ticks_years_full = x_ticks_hours_full / (365 * 24)
+    axes[2][0].set_xticks(x_ticks_hours_full)
+    axes[2][0].set_xticklabels(np.round(x_ticks_years_full, 2))
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.92)
+
+    # Save the plot to a file and store the path
+    adjusted_file_path = f"./adjusted_memory_decay_{title.replace(' ', '_')}.png"
+    plt.savefig(adjusted_file_path)
+    adjusted_plot_paths.append(adjusted_file_path)
+
+    # Close the figure to free up memory
+    plt.close(fig)
+
+adjusted_plot_paths
